e4120071c602ddeac3c24a7f0db619cb34db0435
Vivek Vaishya <vivekvaishya@hotmail.com>
2019-03-23 22:40:34 +0530

    adding GNU-GPL

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,24 @@
+/*This file is part of IceFish Explorer.
+
+    IceFish Explorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    IceFish Explorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with IceFish Explorer. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QIcon>
 
 #include "rfilesystemmodel.h"
-//#include "fileStructures/rdirectorymodel.h"
 #include "fileOperations/rfileoperator.h"
 #include "qmlImageProviders/mimeiconprovider.h"
 #include "qmlImageProviders/pixmappreviewprovider.h"
--- cpp/models/bookmarkInfo/bookmarkinfomodel.cpp
+++ cpp/models/bookmarkInfo/bookmarkinfomodel.cpp
@@ -1,17 +1,17 @@
-/*This file is part of reverse-file-manager.
+/*This file is part of IceFish Explorer.
 
-    reverse-file-manager is free software: you can redistribute it and/or modify
+    IceFish Explorer is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    reverse-file-manager is distributed in the hope that it will be useful,
+    IceFish Explorer is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with reverse-file-manager. If not, see <http://www.gnu.org/licenses/>.
+    along with IceFish Explorer. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "bookmarkinfomodel.h"
